b9dbcef818b88be3cb12d7e63b496e8ac2fca17d
cm921 <sig91308@gmail.com>
2017-02-24 14:28:52 -0500

    not much new

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -2,15 +2,16 @@ package com.company;
 
 public class Methods {
 
-    public static int GDRecurisve(int a, int b){
-        if(a > b){
-
-        }
+    public static int GDIterativly(int a, int b){
         if(a % b > 0)
             return 0;
         int f = a/b;
         int g = a-f*b;
-        if(g ==0);
+        if(g == 0);
             return b;
+    }
+
+    public static int GDRecursive(int a, int b){
+
     }
 }
